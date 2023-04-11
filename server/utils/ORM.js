4df446e48ec7181a62eb2038bc2e906b7dde6a9c
hallerk <kevin.haller@sincrodigital.com>
2023-04-11 13:35:23 -0700

    Saved ORM file

--- server/utils/ORM.js
+++ server/utils/ORM.js
@@ -6,22 +6,21 @@ class ORM {
     }
 
     _setupGlobalORM() {
-        // let orm = new Sequelize(
-        //     process.env["DB_HOST"], 
-        //     process.env["DB_USER"],
-        //     process.env["DB_PASS"],
-        //     {
-        //         dialect: "mysql",
-        //         host: process.env["DB_SERVER"],
-        //         pool: {
-        //             max: 5,
-        //             min: 1
-        //         }
-        //     });
         let orm = new Sequelize({
 			dialect: "sqlite",
 			storage: "localdev.sqlite"
 		});
+        if ("DB_SERVER" in process.env) {
+			orm = new Sequelize(process.env["DB_DATABASE"], process.env["DB_USERNAME"], process.env["DB_PASSWORD"], {
+				dialect: "mysql",
+				host: process.env["DB_SERVER"],
+				pool: {
+					max: 5,
+					min: 1
+				}
+			});
+		}
+
         orm.sync();
         this.orm = orm;
         global.orm = orm;
