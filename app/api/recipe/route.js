323e6425d0ca2c3686709499c880ed3b35c17f01
hallerk <kevin.haller@sincrodigital.com>
2023-06-16 14:01:34 -0700

    update/PR-4 - created remaining recipe routes and restructured directory

--- app/api/recipe/route.js
+++ app/api/recipe/route.js
@@ -1,15 +1,48 @@
 import { NextResponse } from 'next/server';
-import RecipeModel from "../../../models/RecipeModel";
+import RecipeModel from "../../../src/models/RecipeModel";
 
 export async function GET(request) {
-    console.log(request.body)
-    return NextResponse.json({ name: "John Doe" });
+    const { searchParams } = new URL(request.url),
+        id = parseInt(searchParams.get("id")),
+        recipeModel = new RecipeModel();
+        
+    if (id) {
+        return NextResponse.json(
+            await recipeModel.getOne(id)
+        );
+    }
+
+    return NextResponse.json(
+        await recipeModel.getAll()
+    );
 }
 
 export async function POST(request) {
     const requestValues = await request.json(),
-        recipeModel = new RecipeModel(),
-        recipe = await recipeModel.createOne(requestValues);
+        recipeModel = new RecipeModel();
     
-    return NextResponse.json(recipe);
+    return NextResponse.json(
+        await recipeModel.createOne(requestValues)
+    );
+}
+
+export async function PATCH(request) {
+    const { searchParams } = new URL(request.url),
+        id = parseInt(searchParams.get("id")),
+        requestValues = await request.json(),
+        recipeModel = new RecipeModel();
+        
+    await recipeModel.updateOne(id, requestValues);
+
+    return NextResponse.json({sucess: `Recipe id ${id} was updated successfully`});
+}
+
+export async function DELETE(request) {
+    const { searchParams } = new URL(request.url),
+        id = parseInt(searchParams.get("id")),
+        recipeModel = new RecipeModel();
+    
+    await recipeModel.deleteOne(id);
+
+    return NextResponse.json({sucess: `Recipe id ${id} was deleted successfully`});
 }
